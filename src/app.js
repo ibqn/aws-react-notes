e42f550e6c29bab3ffc4c1067268a9ee288c57fb
ibqn <evgen.ibqn@gmail.com>
2021-07-31 17:31:47 +0200

    fetch data from aws

--- src/app.js
+++ src/app.js
@@ -1,24 +1,53 @@
-import logo from './logo.svg'
-import './App.css'
+import { useEffect, useReducer } from 'react'
+import { API } from 'aws-amplify'
+import { listNotes } from './graphql/queries'
 
-function App() {
+const initialState = {
+  notes: [],
+  loading: true,
+  error: false,
+  form: { name: '', description: '' },
+}
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'SET_NOTES':
+      return { ...state, notes: action.notes, loading: false }
+    case 'ERROR':
+      return { ...state, loading: false, error: true }
+    default:
+      return state
+  }
+}
+
+const App = () => {
+  const [state, dispatch] = useReducer(reducer, initialState)
+
+  useEffect(() => {
+    const fetchNotes = async () => {
+      try {
+        const notesData = await API.graphql({ query: listNotes })
+
+        dispatch({ type: 'SET_NOTES', notes: notesData.data.listNotes.items })
+      } catch (error) {
+        console.log('error: ', error)
+        dispatch({ type: 'ERROR' })
+      }
+    }
+
+    const notes = [{ id: 'foo', name: 'Hi there!' }]
+    dispatch({ type: 'SET_NOTES', notes })
+    fetchNotes()
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div>app</div>
+      {JSON.stringify(state)}
+      {state?.notes?.map((note) => (
+        <p key={note.id}>{note.name}</p>
+      ))}
+    </>
   )
 }
 
