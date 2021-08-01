40758f72e0aab3e8dc01547824542094e091b6f9
ibqn <evgen.ibqn@gmail.com>
2021-08-01 17:52:16 +0200

    wip work on form styles

--- src/app.js
+++ src/app.js
@@ -2,7 +2,9 @@ import { useEffect, useReducer } from 'react'
 import { API } from 'aws-amplify'
 import { listNotes as LIST_NOTES } from './graphql/queries'
 import styled from 'styled-components'
-import { List } from 'antd'
+import { List, Button, Input } from 'antd'
+import { Formik, Form, Field, ErrorMessage } from 'formik'
+import * as Yup from 'yup'
 
 const initialState = {
   notes: [],
@@ -28,6 +30,20 @@ const ListItem = styled(List.Item)`
 
 const Container = styled.div`
   padding: 20px;
+`
+
+const InputField = styled(Input)``
+
+const Label = styled.label`
+  margin-bottom: 20px;
+`
+const FormBlock = styled.div`
+  height: 60px;
+  margin-bottom: 20px;
+`
+
+const ErrorField = styled.span`
+  color: coral;
 `
 
 const App = () => {
@@ -60,20 +76,41 @@ const App = () => {
   )
 
   return (
-    <>
-      <div>app</div>
-      {/* {JSON.stringify(state)} */}
-      {state?.notes?.map((note) => (
-        <p key={note.id}>{note.name}</p>
-      ))}
-      <Container>
-        <List
-          loading={state.loading}
-          dataSource={state.notes}
-          renderItem={renderItem}
-        ></List>
-      </Container>
-    </>
+    <Container>
+      <Formik
+        initialValues={{ name: '', description: '' }}
+        validationSchema={Yup.object({
+          name: Yup.string().required('name is required'),
+          description: Yup.string().required('description is required'),
+        })}
+        onSubmit={(values) => console.log(values)}
+      >
+        <Form>
+          <fieldset>
+            <FormBlock>
+              <Label htmlFor="name">Name</Label>
+              <Field name="name" as={InputField} />
+              <ErrorMessage name="name" component={ErrorField} />
+            </FormBlock>
+
+            <FormBlock>
+              <Label htmlFor="description">Description</Label>
+              <Field name="description" as={InputField} />
+              <ErrorMessage name="description" component={ErrorField} />
+            </FormBlock>
+
+            <Button type="primary" htmlType="submit">
+              Create Note
+            </Button>
+          </fieldset>
+        </Form>
+      </Formik>
+      <List
+        loading={state.loading}
+        dataSource={state.notes}
+        renderItem={renderItem}
+      ></List>
+    </Container>
   )
 }
 
